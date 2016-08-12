5580f8af0236a254671741a790f62c2dacab7858
Keven <keven@adagiolabs.fr>
2016-08-12 11:18:33 +0200

    Added wildcard matcher

---
+++ src/WildcardMatcher.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Mark\PatternMatcher;
+
+final class WildcardMatcher extends RegexMatcher
+{
+    const SAFE_STAR          = '__SAFE_STAR_WILCARD__';
+    const SAFE_QUESTION_MARK = '__SAFE_QUESTION_MARK_WILCARD__';
+
+    /**
+     *
+     * @var string
+     */
+    private $pattern;
+
+    /**
+     *
+     * @param string $pattern
+     */
+    public function __construct($pattern)
+    {
+        $this->pattern = $pattern;
+
+        parent::__construct($this->compilePatternToRegex($pattern));
+    }
+
+    /**
+     *
+     * @param type $pattern
+     *
+     * @return string Regex pattern
+     */
+    private function compilePatternToRegex($pattern)
+    {
+        // Escape wildcards
+        $safePattern = strtr($pattern, [
+            '*' => self::SAFE_STAR,
+            '?' => self::SAFE_QUESTION_MARK,
+        ]);
+
+        // Escape preg special chars
+        $escapedPattern = preg_quote($safePattern);
+
+        // Match wildcards
+        return '#^'.strtr($escapedPattern, [
+            self::SAFE_STAR => '.*',
+            self::SAFE_QUESTION_MARK => '.',
+        ]).'$#';
+    }
+}
